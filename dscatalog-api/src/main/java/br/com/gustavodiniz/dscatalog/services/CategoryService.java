c633fdf9bcd3fc69f6b2cdc05f1fd3a951da8f7a
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-01-18 21:36:06 -0300

    created exceptions

--- dscatalog-api/src/main/java/br/com/gustavodiniz/dscatalog/services/CategoryService.java
+++ dscatalog-api/src/main/java/br/com/gustavodiniz/dscatalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package br.com.gustavodiniz.dscatalog.services;
 import br.com.gustavodiniz.dscatalog.dto.CategoryDTO;
 import br.com.gustavodiniz.dscatalog.entities.Category;
 import br.com.gustavodiniz.dscatalog.repositories.CategoryRepository;
+import br.com.gustavodiniz.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,5 +22,12 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
         return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
     }
 }
