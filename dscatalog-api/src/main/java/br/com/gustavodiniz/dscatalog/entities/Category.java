16f99713eff30f9095d303bcdd21c5f51bc448d6
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-01-19 19:24:08 -0300

    created entity product

--- dscatalog-api/src/main/java/br/com/gustavodiniz/dscatalog/entities/Category.java
+++ dscatalog-api/src/main/java/br/com/gustavodiniz/dscatalog/entities/Category.java
@@ -14,6 +14,7 @@ public class Category implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
