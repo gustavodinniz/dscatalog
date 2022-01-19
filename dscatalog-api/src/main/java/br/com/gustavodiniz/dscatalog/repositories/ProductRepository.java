3f05903b39604b6917c88322f5b38aaf638a6608
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-01-19 20:12:15 -0300

    product repository, service, resource, GET ok

---
+++ dscatalog-api/src/main/java/br/com/gustavodiniz/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package br.com.gustavodiniz.dscatalog.repositories;
+
+import br.com.gustavodiniz.dscatalog.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
