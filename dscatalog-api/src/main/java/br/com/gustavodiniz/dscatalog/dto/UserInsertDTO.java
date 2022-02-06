aba04e13efad1615c1a4b040f1efdd2469051608
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-02-05 23:49:54 -0300

    custom constraintValidator

--- dscatalog-api/src/main/java/br/com/gustavodiniz/dscatalog/dto/UserInsertDTO.java
+++ dscatalog-api/src/main/java/br/com/gustavodiniz/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package br.com.gustavodiniz.dscatalog.dto;
 
+import br.com.gustavodiniz.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
     private static final long serialVersionUID = 1L;
---
+++ dscatalog-api/src/main/java/br/com/gustavodiniz/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,41 @@
+package br.com.gustavodiniz.dscatalog.services.validation;
+
+import br.com.gustavodiniz.dscatalog.dto.UserInsertDTO;
+import br.com.gustavodiniz.dscatalog.entities.User;
+import br.com.gustavodiniz.dscatalog.repositories.UserRepository;
+import br.com.gustavodiniz.dscatalog.resources.exceptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = repository.findByEmail(dto.getEmail());
+
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email already exists"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
